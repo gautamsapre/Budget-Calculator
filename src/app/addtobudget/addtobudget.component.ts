11f5b90c3d571e7b1762a15b94734ce16ad45650
Gautam Sapre <gautamsapre@gmail.com>
2020-11-08 17:04:52 -0600

    UI design complete

--- src/app/addtobudget/addtobudget.component.ts
+++ src/app/addtobudget/addtobudget.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-addtobudget',
@@ -10,6 +11,9 @@ export class AddtobudgetComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onSubmit(form: NgForm){
+    console.log(form);
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { AddtobudgetComponent } from './addtobudget/addtobudget.component';
 import { BudgetlistComponent } from './budgetlist/budgetlist.component';
+import { ItemCardComponent } from './budgetlist/item-card/item-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
     AddtobudgetComponent,
-    BudgetlistComponent
+    BudgetlistComponent,
+    ItemCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
